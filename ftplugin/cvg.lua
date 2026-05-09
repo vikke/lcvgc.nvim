e875c9db5e217a6de1f445c3f78a73536c81e8c3
MATSUNAGA Ichiro <vikke.bsd@gmail.com>
2026-05-09 17:59:06 +0900

    feat(ftplugin): cvg filetype に commentstring/comments/formatoptions を追加 (#14)
    * chore: ftplugin/cvg.lua にコメント整形オプション追加用ブランチ作成
    
    ftplugin/cvg.lua に以下を追加する:
    - commentstring (// %s)
    - comments (s1:/*,mb:*,ex:*/,://)
    - formatoptions に croqj を追加
    
    main 既存のインデント設定 (タブ・幅4) はそのまま維持する。
    
    * feat: cvg ftplugin にコメント整形オプションを追加
    
    ftplugin/cvg.lua に以下を追加し、DSL のコメント体験を改善する。
    インデント設定 (タブ・幅4) は変更しない。
    
    - commentstring='// %s' でコメントトグル系プラグインが
      // 行コメント形式で動くようにする
    - comments='s1:/*,mb:*,ex:*/,://' で /* */ ブロックコメントの
      開始/中間/終了マーカを Vim に明示
    - formatoptions に croqj を追加し、ブロックコメント中の
      <Enter>/o/O での継続、gq での整形、J でのマーカ除去を有効化
    
    これらは Neovim デフォルトに含まれない (もしくはバージョン依存
    の) 設定であり、明示することでリポジトリで挙動を確定させる。

--- ftplugin/cvg.lua
+++ ftplugin/cvg.lua
@@ -5,3 +5,13 @@ vim.bo.expandtab = false
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
+
+-- コメント設定: // 行コメント, /* */ ブロックコメント
+-- Comment settings: // line comment, /* */ block comment
+vim.bo.commentstring = '// %s'
+vim.bo.comments = 's1:/*,mb:*,ex:*/,://'
+
+-- 自動整形フラグ
+-- r: <Enter> でコメント継続, o: o/O でコメント継続
+-- q: gq でコメント整形, j: J でのコメント結合をスマートに
+vim.opt_local.formatoptions:append('croqj')
